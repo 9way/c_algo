53cd4e8eaed20051c70fe4af4453a6c7067e21ae
Keric Kuo <kerickuo@gmail.com>
2024-03-10 16:16:45 +0800

    update maxheap

--- maxheap/maxheap.c
+++ maxheap/maxheap.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+#define MAX 1000
+
+
+void printHeap(int arr[], int size)
+{
+    printf("size: %d\n", size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 
 void heapify(int arr[], int n, int i)
 {
@@ -30,8 +43,36 @@ void heapify(int arr[], int n, int i)
     return;
 }
 
+void _insertNode(int arr[], unsigned int index)
+{
+    unsigned parent = (index - 1) / 2;
+    
+    printf("parent: %d, index: %d\n", parent, index);
+    if (arr[parent] < arr[index])
+    {
+        int tmp = arr[parent];
+        arr[parent] = arr[index];
+        arr[index] = tmp;
 
-void maxheap(int arr[], int n)
+        if (parent > 0)
+        {
+            _insertNode(arr, parent);
+        }
+    }
+}
+
+void insertNode(int arr[], unsigned int* n, int Key)
+{
+    printf("%u\n", *n);
+    *n += 1;
+    arr[*n - 1] = Key;
+    printf("%u\n", *n);
+    printHeap(arr, *n);
+    _insertNode(arr, *n - 1);
+}
+
+
+void maxheap(int arr[], unsigned int n)
 {
     for (int i = n / 2 - 1; i >=0; i--)
     {
@@ -41,7 +82,7 @@ void maxheap(int arr[], int n)
 
 int main(void)
 {
-    int arr[] = {20, 18, 5, 14, 3, 2, 1, 11, 19, 20};
+    int arr[MAX] = {20, 18, 5, 14, 3, 2, 1, 11, 19, 20};
     unsigned int n = 10;
 
     for (unsigned int i = 0; i < n; i++)
@@ -54,6 +95,16 @@ int main(void)
     maxheap(arr, n);
 
     printf("max heap: ");
+    for (unsigned int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int key = 21;
+    insertNode(arr, &n, key);
+
+    printf("insert 21: ");
     for (unsigned int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
